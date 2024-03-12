7a1661760d2993e83e95ad5eea42a159ad6105bf
陈富硕 <95668838+ChenFushuo@users.noreply.github.com>
2024-03-12 10:37:16 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: 'Vue', url: '/tag/Vue' },
   HEO_HERO_CATEGORY_2: { title: 'React', url: '/tag/React' },
-  HEO_HERO_CATEGORY_3: { title: '技术分享', url: '/tag/技术分享' },
+  HEO_HERO_CATEGORY_3: { title: '技术分享', url: '/技术分享' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
